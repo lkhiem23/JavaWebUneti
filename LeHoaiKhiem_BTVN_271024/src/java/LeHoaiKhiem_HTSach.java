0d05dc8aecf43f6f45ee8b223769d317235f6a36
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-11-02 23:59:50 +0700

    Update LeHoaiKhiem_HTSach.java

--- LeHoaiKhiem_BTVN_271024/src/java/LeHoaiKhiem_HTSach.java
+++ LeHoaiKhiem_BTVN_271024/src/java/LeHoaiKhiem_HTSach.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.sql.*; // Đảm bảo nhập khẩu đúng các lớp 
+import java.sql.*;  
 
 /**
  *
