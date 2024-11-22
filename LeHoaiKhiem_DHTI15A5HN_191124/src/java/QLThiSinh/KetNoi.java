75b2555386a878282723e732db61b3a4801271c5
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-11-22 20:44:54 +0700

    Update KetNoi.java

--- LeHoaiKhiem_DHTI15A5HN_191124/src/java/QLThiSinh/KetNoi.java
+++ LeHoaiKhiem_DHTI15A5HN_191124/src/java/QLThiSinh/KetNoi.java
@@ -7,19 +7,9 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ADMIN
- */
 public class KetNoi {
     public static Connection KNCSDL() throws SQLException {
         try {
-            // Nạp trình điều khiển
             Class.forName("com.mysql.jdbc.Driver");
             Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/qlthisinh","root","");
             return c;
