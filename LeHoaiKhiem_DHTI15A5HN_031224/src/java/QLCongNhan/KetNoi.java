1c53edd48fc0074f04c0267ae85710fe236d37d6
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-12-06 23:57:22 +0700

    Update KetNoi.java

--- LeHoaiKhiem_DHTI15A5HN_031224/src/java/QLCongNhan/KetNoi.java
+++ LeHoaiKhiem_DHTI15A5HN_031224/src/java/QLCongNhan/KetNoi.java
@@ -6,15 +6,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author ADMIN
- */
 public class KetNoi {
     public static Connection KNCSDL() throws SQLException {
         try {
