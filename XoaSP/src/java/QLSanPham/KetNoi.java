79e54369e3ee7560d4d9a89a62451692feb902ef
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-12-17 18:14:11 +0700

    Update KetNoi.java

--- XoaSP/src/java/QLSanPham/KetNoi.java
+++ XoaSP/src/java/QLSanPham/KetNoi.java
@@ -1,9 +1,5 @@
 package QLSanPham;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 import java.sql.*;
 import java.util.*;
 import java.util.logging.Level;
