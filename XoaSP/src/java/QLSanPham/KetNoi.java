2b92eec9665b7ffcb1fd9af1420bb72de97cce0f
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-12-17 18:14:03 +0700

    Update KetNoi.java

--- XoaSP/src/java/QLSanPham/KetNoi.java
+++ XoaSP/src/java/QLSanPham/KetNoi.java
@@ -8,10 +8,7 @@ import java.sql.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/**
- *
- * @author ADMIN
- */
+
 public class KetNoi {
     public static Connection KNCSDL() throws SQLException{
         try {
