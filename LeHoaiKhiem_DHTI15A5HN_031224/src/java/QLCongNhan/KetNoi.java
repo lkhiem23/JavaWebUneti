a6c10d50a3fcaa6ad85fe47309c624b3555c9b10
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-12-27 13:53:19 +0700

    Update KetNoi.java

--- LeHoaiKhiem_DHTI15A5HN_031224/src/java/QLCongNhan/KetNoi.java
+++ LeHoaiKhiem_DHTI15A5HN_031224/src/java/QLCongNhan/KetNoi.java
@@ -9,7 +9,6 @@ import java.util.logging.Logger;
 public class KetNoi {
     public static Connection KNCSDL() throws SQLException {
         try {
-            // Nạp trình điều khiển
             Class.forName("com.mysql.jdbc.Driver");
             Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/qlcongnhan", "root", "");
             return c;
